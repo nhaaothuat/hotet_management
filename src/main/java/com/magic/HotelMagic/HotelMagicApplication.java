3201d5eae1c03a7343790f5c910c2450fe10c2f9
M2 C1 <hienphannguyenxuan@gmail.com>
2025-05-14 09:10:35 +0700

    new project

---
+++ src/main/java/com/magic/HotelMagic/HotelMagicApplication.java
@@ -0,0 +1,13 @@
+package com.magic.HotelMagic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HotelMagicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HotelMagicApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/magic/HotelMagic/HotelMagicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.magic.HotelMagic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HotelMagicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
